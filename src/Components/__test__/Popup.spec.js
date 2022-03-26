71175fec436f4698e475df6b66e7abb3a35458a9
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-03-26 19:01:12 +0300

    Create functions before creating popup functions

--- src/Components/__test__/Popup.spec.js
+++ src/Components/__test__/Popup.spec.js
@@ -5,41 +5,34 @@ import App from '../../App'
 
 const setup = () => render(<App />)
 const getPopup = () => screen.getByTestId('popup')
+const getApplyButton = () => screen.getByRole('button', { name: /Apply/i })
+const getCloseButton = () => screen.getByRole('button', { name: /Close/i })
 
 afterEach(() => cleanup)
 
 test('Popup renders correctly', () => {
   setup()
-
   expect(getPopup()).toBeInTheDocument()
 })
 
 test('Popup is have "Apply" button', () => {
   setup()
-  const popupElmApplyButton = screen.getByTestId('popup-apply')
-  expect(popupElmApplyButton).toBeInTheDocument()
+  expect(getApplyButton()).toBeInTheDocument()
 })
 
 test('Popup is have "Close" button', () => {
   setup()
-  const popupElmCloseButton = screen.getByTestId('popup-close')
-  expect(popupElmCloseButton).toBeInTheDocument()
-})
-
-test('Popup is closing after clicking "Close" button', () => {
-  setup()
-  const popupElmCloseButton = screen.getByRole('button', {
-    name: /Close/i
-  })
-  userEvent.click(popupElmCloseButton)
-  expect(getPopup()).toHaveClass('hidden')
+  expect(getCloseButton()).toBeInTheDocument()
 })
 
 test("Popup's body changes after clicking 'Apply' button", () => {
   setup()
-  const popupElmApplyButton = screen.getByRole('button', {
-    name: /Apply/i
-  })
-  userEvent.click(popupElmApplyButton)
+  userEvent.click(getApplyButton())
   expect(getPopup()).toHaveTextContent(/You have successfully applied./i)
+})
+
+test('Popup is closing after clicking "Close" button', () => {
+  setup()
+  userEvent.click(getCloseButton())
+  expect(getPopup()).toHaveClass('hidden')
 })
