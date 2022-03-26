a6a07869da821912b9622e13d8d79da5c70a8e24
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-03-26 18:44:35 +0300

    set testid

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Popup from './Components/Popup'
 
 function App() {
   return (
-    <div className="w-full relative p-5 m-0 min-h-screen flex flex-wrap gap-10 justify-center content-center bg-zinc-900">
+    <div
+      data-testid="test-body"
+      className="w-full relative p-5 m-0 min-h-screen flex flex-wrap gap-10 justify-center content-center bg-zinc-900"
+    >
       <header className="absolute text-xl left-5 top-5 text-zinc-300 font-semibold">
         <h1 className="p-5 pt-4 bg-zinc-300 bg-opacity-10 rounded-xl">
           Testing POC's
