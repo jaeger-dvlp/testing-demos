64703a93efc85ff5bb80d608843fe00ca7631b91
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-03-26 16:13:06 +0300

    Import popup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import Popup from './Components/Popup'
 
 function App() {
-  return <div className="App">Testing..</div>
+  return (
+    <div className="w-full relative p-5 m-0 min-h-screen flex flex-wrap gap-10 justify-center content-center bg-zinc-900">
+      <header className="absolute text-xl left-5 top-5 text-zinc-300 font-semibold">
+        <h1 className="p-5 pt-4 bg-zinc-300 bg-opacity-10 rounded-xl">
+          Testing POC's
+        </h1>
+      </header>
+      <Popup />
+    </div>
+  )
 }
 
 export default App
